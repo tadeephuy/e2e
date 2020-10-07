93ae629ec041677b41769a2232348d2df09921db
TaDucHuy <tdh512194@gmail.com>
2020-10-07 16:26:31 +0700

    add trainer class

--- scripts/train.py
+++ scripts/train.py
@@ -5,82 +5,93 @@ import torch
 from ..core.ddpg import Agent, OUNoise
 from ..core.environment import Environment
 
+class Trainer:
+    def __init__(self, config, critic_arch, actor_arch, classifier):
+        self.config = config
+        self.critic_arch, self.actor_arch, self.classifier = critic_arch, actor_arch, classifier
 
-# Define arguments
-
-CRITIC_ARCH = None
-ACTOR_ARCH = None
-CRITIC_SIZE = None
-ACTOR_SIZE = None
-ACTION_SIZE = None
-CRITIC_LR = None
-ACTOR_LR = None
-GAMMA = None
-TAU = None
-
-IMG_DIR = None
-IMG_SIZE = 320
-BLOB_SIZE = 5
-DONE_THRESHOLD = 0.5
-
-BATCH_SIZE = 64
-N_EPISODE = 50
-MAX_LENGTH_EPISODE = 20
-
-# Define variables
-
-agent = Agent(
-    critic_arch=CRITIC_ARCH, 
-    actor_arch=ACTOR_ARCH, 
-    critic_hidden_size=CRITIC_SIZE, 
-    actor_hidden_size=ACTOR_SIZE, 
-    action_size=ACTION_SIZE,
-    critic_learning_rate=CRITIC_LR,
-    actor_learning_rate=ACTOR_LR,
-    gamma=GAMMA,
-    tau=TAU
-    )
-
-env = Environment(
-    agent=agent,
-    classifier=classifier,
-    img_dir=IMG_DIR,
-    img_size=IMG_SIZE,
-    blob_size=BLOB_SIZE,
-    done_threshold=DONE_THRESHOLD
-    )
-
-noise = OUNoise(ACTION_SIZE)
-
-batch_size = BATCH_SIZE
-rewards = []
-avg_rewards = []
-
-for episode in range(N_EPISODE):
-    state = env.reset()
-    noise.reset()
-    episode_reward = 0
-
-    for step in range(MAX_LENGTH_EPISODE):
-        action = agent.get_action(state)
-        action = noise.get_action(action=action, t=step)
-        new_state, reward, done, info = env.step(action)
-        agent.memory.push(state, action, reward, new_state, done)
-
-        if len(agent.memory) > batch_size:
-            agent.update(batch_size)
-
-        state = new_state
-        episode_reward += reward
-
-        if done:
-            print(f"Done episode {episode:<3} with reward of: {episode_reward:.2f}, avg reward: {np.mean(rewards)}")
-            break
+        self.initialize(self.config)
     
-    rewards.append(episode_reward)
-    avg_rewards.append(np.mean(rewards))
+    def initialize(self, config):
+        self.config = config
 
-torch.save(agent.critic.state_dict(), 'critic.pth')
-torch.save(agent.actor.state_dict(), 'actor.pth')
-torch.save(agent.critic_target.state_dict(), 'critic_target.pth')
-torch.save(agent.actor_target.state_dict(), 'actor_target.pth')
+        # initialize agent
+        self.agent = Agent(
+            critic_arch         = self.critic_arch, 
+            actor_arch          = self.actor_arch, 
+            critic_hidden_size  = self.config['CRITIC_SIZE'], 
+            actor_hidden_size   = self.config['ACTOR_SIZE'], 
+            action_size         = self.config['ACTION_SIZE'],
+            critic_learning_rate= self.config['CRITIC_LR'],
+            actor_learning_rate = self.config['ACTOR_LR'],
+            gamma               = self.config['GAMMA'],
+            tau                 = self.config['TAU']
+            )
+
+        # intialize environment
+        self.env = Environment(
+            agent               = self.agent,
+            classifier          = self.classifier,
+            img_dir             = self.config['IMG_PATH'],
+            img_size            = self.config['IMG_SIZE'],
+            blob_size           = self.config['BLOB_SIZE'],
+            done_threshold      = self.config['DONE_THRESHOLD']
+            )
+        
+        self.noise = OUNoise(self.config['ACTION_SIZE'])
+
+    def run(self):
+        batch_size = self.config['BATCH_SIZE']
+        rewards = []
+        avg_rewards = []
+
+        for episode in range(self.config['N_EPISODES']):
+            state = self.env.reset()
+            self.noise.reset()
+            episode_reward = 0
+
+            for step in range(self.config['MAX_LENGTH_EPISODE']):
+                action = agent.get_action(state)
+                action = self.noise.get_action(action=action, t=step)
+                new_state, reward, done, info = self.env.step(action)
+                self.agent.memory.push(state, action, reward, new_state, done)
+
+                if len(self.agent.memory) > batch_size:
+                    self.agent.update(batch_size)
+
+                state = new_state
+                episode_reward += reward
+
+                if done:
+                    print(f"Done episode {episode:<3} with reward of: {episode_reward:.2f}, avg reward: {np.mean(rewards)}")
+                    break
+
+            rewards.append(episode_reward)
+            avg_rewards.append(np.mean(rewards))
+
+    def save(self, name=None):
+        """
+        specify `name` to override experiment name in config
+        """
+        name = name or self.config['NAME']
+        state_dict = {
+            'critic': self.agent.critic.state_dict(),
+            'actor': self.agent.actor.state_dict(),
+            'critic_target': self.agent.critic_target(),
+            'actor_target': self.agent.actor_target()
+        }
+
+        torch.save(state_dict, f'{self.config['WEIGHT_PATH']}/{name}.pth')
+
+    def load(self, name=None):
+        """
+        specify `name` to override experiment name in config
+        """
+        name = name or self.config['NAME']
+        state_dict = torch.load(f'{self.config['WEIGHT_PATH']}/{name}.pth')
+
+        self.agent.critic.load_state_dict(state_dict['critic'])
+        self.agent.actor.load_state_dict(state_dict['actor'])
+        self.agent.critic_target.load_state_dict(state_dict['critic_target'])
+        self.agent.actor_target.load_state_dict(state_dict['actor_target'])
+        
